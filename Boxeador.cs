bbdb49d432c21f3d3fdeaf98cf893c3163fec3ec
TOMAS NEIMERMAN GROSSO <47496739@ort>
2023-04-19 09:03:53 -0300

    2

--- Boxeador.cs
+++ Boxeador.cs
@@ -1,8 +1,28 @@
 class Boxeador{
+    public string Nombre{get;set;}
+    public string Pais{get;set;}
+    public int Peso{get;set;}
+    public int PotenciaGolpes{get;set;}
+    public int VelocidadPiernas{get;set;}
+    public Boxeador(string nom, string pais, int peso, int pg, int vp){
+        Nombre = nom;
+        Pais = pais;
+        Peso = peso;
+        PotenciaGolpes = pg;
+        VelocidadPiernas = vp;
+    }
+    public int ObtenerSkill(){
+        int vp = Convert.ToInt32(VelocidadPiernas*0.6);
+        int pg = Convert.ToInt32(PotenciaGolpes*0.8);
+        Random r = new Random();
+        int numeroazar = r.Next(1,10);
+        int skill = numeroazar+pg+vp;
+        return skill;
+
+    }
 
 
 
 
 
-    
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,47 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string e = Environment.NewLine;
+        int menu = 0;
+        Boxeador box1 = new Boxeador("","", 0, 0 ,0), box2 = new Boxeador("","",0,0,0);
+        while(menu != 4){
+            menu = IngresarInt($"Elija una opcion {e}1. Cargar Datos Boxeador 1 {e}2. Cargar Datos Boxeador 2 {e}3. Pelear!{e}4. Salir");
+            switch(menu){
+                case 1:
+                DatosBoxeador(ref box1, menu);
+                break;
+                case 2:
+                DatosBoxeador(ref box2, menu);
+                break;
+                case 3:
+                Pelear(box1,box2);
+                break;
+            }
+        }
+    }
+    static void DatosBoxeador(ref Boxeador box, int num){
+        string nom, pais;
+        int peso,vp,pg;
+        nom = IngresarString("Ingresa el nombre del boxeador "+ num);
+        pais = IngresarString("Ingrese el pais del boxeador "+ num);
+        peso = IngresarInt("Ingrese el peso del boxeador "+ num);
+        vp = IngresarInt("Ingrese la velocidad del boxeador "+ num);
+        pg = IngresarInt("Ingrese la potencia del boxeador "+ num);
+        box = new Boxeador(nom,pais,peso,pg,vp);
+    } 
+    static void Pelear(Boxeador box1, Boxeador box2){
+        int skillbox1 = box1.ObtenerSkill();
+        int skillbox2 = box2.ObtenerSkill();
+
+    }
+    static int IngresarInt(string m){
+        int d;
+        Console.WriteLine(m);
+        return d = int.Parse(Console.ReadLine());
+    }
+    static string IngresarString(string m){
+        string d;
+        Console.WriteLine(m);
+        return d = Console.ReadLine();
     }
 }
